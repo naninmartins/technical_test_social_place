ea31a749cb982088fd126a3879ff357c09049acf
Ernani Martins <naninmartins@gmail.com>
2019-06-14 19:08:17 -0300

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+#git init
 git checkout build
 git add -A
 git commit -m 'deploy'
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f origin build
+ git push origin build
 
 cd -
